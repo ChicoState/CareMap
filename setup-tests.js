b83cb4e39826d5025ee78aef8c3141afac34824a
Gary Her <gher3@mail.csuchico.edu>
2019-12-15 17:21:32 -0800

    Added setup for tests

---
+++ setup-tests.js
@@ -0,0 +1,11 @@
+import 'react-native';
+import 'jest-enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+//import Enzyme from 'enzyme';
+
+/**
+ * Set up Enzyme to mount to DOM, simulate events,
+ * and inspect the DOM in tests.
+ */
+Enzyme.configure({ adapter: new Adapter() });
